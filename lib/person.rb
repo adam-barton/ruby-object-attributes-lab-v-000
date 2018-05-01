bea044fb6f311f73982d79e252256b518880c618
Adam Barton <adam.barton333@gmail.com>
2018-05-01 04:05:35 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,10 +8,20 @@ def name
   @name
 end
 
+def job=(persons_job)
+    @job = persons_job
+  end
+ 
+  def job 
+    @bjob
+  end
+
 end
 
 beyonce = Person.new
 
 beyonce.name = ("Beyonce")
+beyonce.job = ("Singer")
 
 puts beyonce.name
+puts beyonce.job
